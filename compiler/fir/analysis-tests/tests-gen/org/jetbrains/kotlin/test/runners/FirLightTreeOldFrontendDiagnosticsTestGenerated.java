7cd395542bcecb5fd6c6e058e37266e789661dc0
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2023-11-27 13:37:56 +0200

    [FIR] Ensure the case from KT-63741 works
    ^KT-63741

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
@@ -26451,6 +26451,12 @@ public class FirLightTreeOldFrontendDiagnosticsTestGenerated extends AbstractFir
             }
 
             @Test
+            @TestMetadata("intersectionOverrideWithProtectedAbstract.kt")
+            public void testIntersectionOverrideWithProtectedAbstract() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/override/intersectionOverrideWithProtectedAbstract.kt");
+            }
+
+            @Test
             @TestMetadata("InvisiblePotentialOverride.kt")
             public void testInvisiblePotentialOverride() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/override/InvisiblePotentialOverride.kt");
