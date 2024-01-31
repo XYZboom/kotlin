a664876edf4e307358627af68167f0024e864259
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2024-01-31 08:37:47 +0100

    K2: reproduce KT-65410

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
@@ -22758,6 +22758,12 @@ public class FirLightTreeOldFrontendDiagnosticsTestGenerated extends AbstractFir
                 }
 
                 @Test
+                @TestMetadata("removeAt.kt")
+                public void testRemoveAt() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/j+k/primitiveOverrides/removeAt.kt");
+                }
+
+                @Test
                 @TestMetadata("specializedMap.kt")
                 public void testSpecializedMap() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/j+k/primitiveOverrides/specializedMap.kt");
